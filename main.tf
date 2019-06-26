dff82e2fed6121d52cd0e575c3f7fe337d51d713
Alex Basista <alex@Alexs-MBP-3.attlocal.net>
2019-06-26 11:42:27 -0400

    initial

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 0.12.2"
+}
+
+variable "starks" {
+    description = "list of the Stark first names"
+    default     = ["ned", "catelyn", "robb", "sansa", "arya", "bran", "rickon", "rickard", "lyanna"]
+}
+
+variable "lannisters" {
+    description = "list of Lannister first names"
+    default     = ["tywin", "joanna", "jaime", "cersei", "tyrion", "lancil", "tytos"]
+}
+
+resource "random_shuffle" "stark" {
+    input = [
+        for stark in var.starks:
+        upper(stark)
+    ]
+}
+
+resource "random_shuffle" "lannister" {
+    input = [
+        for lannister in var.lannisters:
+        upper(lannister)
+    ]
+}
+
+output "starks" {
+    value = random_shuffle.stark.result
+}
+
+output "lannister" {
+    value = random_shuffle.lannister.result
+}
