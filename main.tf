39817ab78e6bc8793f396291bed8d232e91a5f64
Alex Basista <alex.basista@gmail.com>
2021-03-18 16:47:22 -0400

    test update

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 variable "starks" {
     type        = list(string)
     description = "List of Stark first names."
-    default     = ["ned", "catelyn", "robb", "sansa", "arya", "bran", "rickon", "rickard", "lyanna", "test-stark"]
+    default     = ["ned", "catelyn", "robb", "sansa", "arya", "bran", "rickon", "rickard", "lyanna"]
 }
 
 variable "lannisters" {
     type        = list(string)
     description = "list of Lannister first names."
-    default     = ["tywin", "joanna", "jaime", "cersei", "tyrion", "lancel", "tytos", "test-lannister"]
+    default     = ["tywin", "joanna", "jaime", "cersei", "tyrion", "lancel", "tytos"]
 }
 
 variable "tyrells" {
