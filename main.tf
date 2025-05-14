23e761f2b3602d1201f253e8fee1542c05c996c4
alexbasista <abasista@hashicorp.com>
2025-05-14 15:00:54 -0400

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+#---------------------------------------------------------------
+# Provider
+#---------------------------------------------------------------
+terraform {
+  required_providers {
+    random = {
+      source = "tfeaws.abasista.sbx.hashidemos.io/banana/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+# terraform {
+#   required_providers {
+#     random = {
+#       source = "app.terraform.io/abasista-tfc/random"
+#       version = "3.7.2"
+#     }
+#   }
+# }
+
+
+#---------------------------------------------------------------
+# Resources
+#---------------------------------------------------------------
 resource "random_shuffle" "stark" {
     input = [
         for stark in var.starks:
@@ -12,14 +37,15 @@ resource "random_shuffle" "lannister" {
     ]
 }
 
-resource "null_resource" "tyrells" {
-    for_each = var.tyrells
-
-    provisioner "local-exec" {
-      command = "echo ${each.value.firstname}; echo ${each.value.lastname}"
-    }
+resource "random_shuffle" "tyrell" {
+    input = [
+        for tyrell in var.tyrells:
+        upper(tyrell)
+    ]
 }
 
 resource "random_pet" "change_1" {}
 
 resource "random_pet" "change_2" {}
+
+resource "random_pet" "change_3" {}
