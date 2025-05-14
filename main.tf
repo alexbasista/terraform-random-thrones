6189364774c4dd26230e48c03f885bdab61b257e
alexbasista <abasista@hashicorp.com>
2025-05-14 15:07:49 -0400

    change provider source

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 #---------------------------------------------------------------
 # Provider
 #---------------------------------------------------------------
-terraform {
-  required_providers {
-    random = {
-      source  = "tfeaws.abasista.sbx.hashidemos.io/banana/random"
-      version = "3.7.2"
-    }
-  }
-}
-
 # terraform {
 #   required_providers {
 #     random = {
-#       source = "app.terraform.io/abasista-tfc/random"
+#       source  = "tfeaws.abasista.sbx.hashidemos.io/banana/random"
 #       version = "3.7.2"
 #     }
 #   }
 # }
+
+terraform {
+  required_providers {
+    random = {
+      source = "app.terraform.io/abasista-tfc/random"
+      version = "3.7.2"
+    }
+  }
+}
 
 
 #---------------------------------------------------------------
